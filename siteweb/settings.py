86b4ec1a759dec2e59bde0fce719d2342732a9de
Alban <albancadic@gmail.com>
2023-06-26 16:17:48 +0200

    s

--- siteweb/settings.py
+++ siteweb/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-from .email import*
+from .mail import*
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
