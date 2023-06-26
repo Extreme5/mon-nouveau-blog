589c94a3d9919273318c62e610ee1d0290894a5d
Alban <albancadic@gmail.com>
2023-06-26 11:01:20 +0200

    d

--- siteweb/email.py
+++ siteweb/email.py
@@ -1,5 +1,5 @@
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_HOST_USER = "albancadic@gmail.com"
-EMAIL_HOST_PASSWORD = "Wse'rfvd4"
+EMAIL_HOST_USER = "#"
+EMAIL_HOST_PASSWORD = "#"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
